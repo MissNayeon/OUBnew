c7aff935117a0709d9e9248e17ecdafd3f29b01f
Miss Naeyon <69038445+MissNayeon@users.noreply.github.com>
2020-08-03 10:49:03 +0700

    Update fakeload.py

--- userbot/modules/fakeload.py
+++ userbot/modules/fakeload.py
@@ -32,7 +32,7 @@ async def typewriter(typew):
     await typew.edit(str(number) + "%   █")
     number += 1
     sleep(0.03)
-    await typew.edit(str(number) + "%   █▎")
+    await typew.edit(str(number) + "%   █▎"
     number += 1
     sleep(0.03)
     await typew.edit(str(number) + "%   █▍")
@@ -314,7 +314,7 @@ async def typewriter(typew):
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
     await typew.edit(
-        "Tokek Goreng Siap di Santap![Pesan DISINI](https://www.google.com/url?sa=t&source=web&rct=j&url=https://m.tokopedia.com/amp/joeluqmany/daging-tokek-gorengbakar-obat-gatal-asma-aids-dll&ved=2ahUKEwjI6bjeocfnAhXmyDgGHXp9CTYQFjADegQIChAB&usg=AOvVaw0JG7Fs8bPq_2rynIS_CtMF&ampcf=1)"
+        "MissNaeyon [Klik](https://github.com/MissNayeon/OUBnew/edit/sql-extended/userbot/modules/fakeload.py=1)"
     )
     # I did it for two hours :D just ctrl+c - crtl+v
 
